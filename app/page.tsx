5694dcfeabf0e22b699be1cedf4552187908d2f3
Shoyayeb <sa.fine@outlook.com>
2023-06-08 16:27:45 +0000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import CityPicker from "@/components/CityPicker";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto">
+      <Text className="text-6xl text-center mb-10 font-bold">Weather Update</Text>
+      <Subtitle className="text-xl text-center">Powered by Nextjs 13, Tailwindcss, Tremor, ChatGPT</Subtitle>
+      <Divider  className="my-10"/>
+      <Card className="background-gradient-to-br from-[#394F68] to-[#183B7E]">
+    <CityPicker />
+      </Card>
+    </Card>
+    </div>
+  )
+}
